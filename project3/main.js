97510b7da8b0ada92f6b8439f85d8129506ba746
yoonbumsu <64895189+yoonbumsu@users.noreply.github.com>
2021-03-04 00:12:49 +0900

    Add files via upload

--- project3/main.js
+++ project3/main.js
@@ -1,12 +1,14 @@
 //from json file
-function loadItems(){
-    return fetch('data/data.json')
-    .then(response => response.json())
-    .then(json => json.items);
-}
 
+function loadItems(){
+    return fetch('data/data.json') //json 경로
+    .then(response => response.json()) //데이터 성공적으로 받아올시
+    .then(json => json.items); 
+}
+//update list 
 function displayItems(items){
     const container = document.querySelector('.items');
+    
     container.innerHTML  = items.map(item => createHtmlString(item)).join('');
 
 }
@@ -20,10 +22,37 @@ function createHtmlString(item){
 
   `;
 }
+function onButtonClick(event , items){
+  const target = event.target;
+  const key = target.dataset.key;
+  const value = target.dataset.value;
+
+  if(key == null || value ==null){
+    return;
+  }
+  updateItems(items , key ,value);
+}
+function updateItems(items,key,value){
+  items.forEach(item => {
+    if(item.dataset[key] ===value){
+      item.classlist.remove('invisible');
+    }else{
+      item.classlist.add('invisible');
+    }
+  });
+}
+
+function setEventListeners(items){
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', event => onButtonClick(event,items));
+  }
+
 //main
 loadItems()
 .then(items => {
-  console.log(items);
+ 
   displayItems(items);
   //  setEventlisteners(items)
 })
